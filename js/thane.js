26d412be2df8e91bbf9c7d5a26b44824572d51ac
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2020-06-27 20:01:28 +0530

    Update thane.js

--- js/thane.js
+++ js/thane.js
@@ -9,7 +9,7 @@ fetch(tha)
     document.querySelector(".stat__item.confirmedtha .number").textContent =
       myData[2];
     document.querySelector(".stat__item.activetha .number").textContent = `${
-      myData[2] - myData[4] - myData[3]
+      myData[2] - myData[4] - myData[3]}`;
     document.querySelector(".stat__item.recoveredtha .number").textContent =
       myData[4];
     document.querySelector(".stat__item.deceasedtha .number").textContent =
